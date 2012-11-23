dc11dee9db1f21b15ba0ad601ab6068ec9e1ce3a
Sam Rijs <sam@dev.local>
2012-11-23 22:22:05 +0100

    test: fill buffer when undecided

--- test.py
+++ test.py
@@ -13,25 +13,29 @@ def printTok(context, token):
 
   global STATE
 
-  def refill(context):
-    if context.sz < 4:
-      context.sz += stdin.readinto(ffi.buffer(context.buf + context.sz, context.back_sz - context.sz))
+  def refill():
+    context.sz += stdin.readinto(ffi.buffer(context.buf + context.sz, context.back_sz - context.sz))
     while context.sz < 4:
-      context.buf[context.sz] = "\0"
+      context.buf[context.sz] = '\0'
       context.sz += 1
 
-  if token.state == 'Fail':
-    print 'Fail, Tok: %s, Char: %s' % (token.type, buf[0])
-    STATE = False
-
-  elif token.state == 'Success':
+  if token.state == 'Success':
     print '%s %s: %s' % (token.type, token.len, ffi.string(context.buf, token.len))
     context.buf += token.len
     context.sz  -= token.len
-    refill(context)
+    if context.sz < 4:
+      refill()
 
   elif token.state == 'Undecided':
-    print 'Undecided'
+    if context.sz < context.back_sz:
+      ffi.buffer(context.back_buf, context.sz)[:] = ffi.buffer(context.buf, context.sz)[:]
+      context.buf = context.back_buf
+      refill()
+    else:
+      STATE = False
+
+  elif token.state == 'Fail':
+    print 'Fail, Tok: %s, Char: %s' % (token.type, buf[0])
     STATE = False
 
   elif token.state == 'End':
@@ -45,5 +49,5 @@ ctx = ffi.new('Ctx *', [4096, 4096, buf, buf])
 
 ctx.sz = stdin.readinto(ffi.buffer(buf, ctx.back_sz))
 
-while STATE == True:
+while STATE:
   Lex.lex(ctx, printTok)
