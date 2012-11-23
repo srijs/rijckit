b7c0024ff4496153d2ee3e1f7a597a217662ba2a
Sam Rijs <sam@dev.local>
2012-11-23 22:34:55 +0100

    dat lines

--- lex.c
+++ lex.c
@@ -4,39 +4,36 @@
 // The main design goals are as follows:
 //
 // 1. To provide a very readable implementation of a lexer function.
-// 2. To be useful in sequential work-flows, as well as systems
-//    working with asynchronous IO and / or threads.
+// 2. To be useful in sequential work-flows, as well as systems working with
+//    asynchronous IO and / or threads.
 // 3. To be completely independent from any external libraries.
-// 4. To perform all data dependent computations as fast as possible
-//    and leave the rest to other software components.
-// 5. To maximize the mean throughput by optimizing the code-paths
-//    for small tokens.
+// 4. To perform all data dependent computations as fast as possible and leave
+//    the rest to other software components.
+// 5. To maximize the mean throughput by optimizing the code-paths for small
+//    tokens.
 // 6. To use no heap space by utilizing continuation-passing-style and
 //    performing allocations solely on the stack.
 
 #include <stddef.h>
 #include <stdbool.h>
 
-// Type-declarations and interfaces of this lexer are found in the
-// `lex.h` header file.
+// Type-declarations and interfaces of this lexer are found in the `lex.h`
+// header file.
 
 #include "lex.h"
 
-// In order to optimize code-paths, we define two convenience macros
-// to give branch-prediction information to the compiler.
+// In order to optimize code-paths, we define two convenience macros to give
+// branch-prediction information to the compiler.
 
 #define likely(x)   (__builtin_expect(x, true))
 #define unlikely(x) (__builtin_expect(x, false))
 
-
-
 // ## Lexemes
 //
-// To provide good readability and understandability, the
-// functionality of the lexer as a whole is factored into its
-// individual lexemes. Each of those is provided as a static function,
-// taking the current context of the lexing process as a parameter
-// and returning the matched token structure.
+// To provide good readability and understandability, the functionality of the
+// lexer as a whole is factored into its individual lexemes. Each of those is
+// provided as a static function, taking the current context of the lexing
+// process as a parameter and returning the matched token structure.
 
 // ### Lexeme: Number
 // Stub.
@@ -59,11 +56,11 @@ static inline Tok number (Ctx *const ctx) {
 //
 // `whitespace ::= ( Space | Tab | NL | CR ) { whitespace }`
 //
-// An identifier is an _arbitrary long_ sequence of alphanumerics
-// and underscore characters.
+// An identifier is an _arbitrary long_ sequence of alphanumerics and underscore
+// characters.
 //
-// Whitespace is an _arbitrary long_ sequence of space, tab,
-// new-line and carriage-return characters.
+// Whitespace is an _arbitrary long_ sequence of space, tab, new-line and
+// carriage-return characters.
 
 static inline bool is_alnum (char c) {
   return ((c >= 'A') & (c <= 'Z')) | ((c >= 'a') & (c <= 'z'))
@@ -105,14 +102,12 @@ static inline Tok identifier_or_whitespace (int type, Ctx *const ctx) {
 //
 // `directive ::= Hash      ( Seq      ) Newline`
 //
-// A string literal is a string of ASCII-identifier between
-// two double quotes.
+// A string literal is a string of ASCII-identifier between two double quotes.
 //
-// A character literal is a string of ASCII-identifier between
-// two single quotes.
+// A character literal is a string of ASCII-identifier between two single quotes.
 //
-// A preprocessor directive is introduced by a hash character (`#`)
-// and end with an unescaped newline.
+// A preprocessor directive is introduced by a hash character (`#`) and end with
+// an unescaped newline.
 
 static inline Tok str_or_char_or_pp (int type, Ctx *const ctx) {
 
@@ -214,16 +209,14 @@ static inline Tok punctuation (Ctx *const ctx) {
 
 }
 
-
-
 // ## Routing
 
 void lex (Ctx *const ctx, const Cont ret) {
 
   // Based on the first character of the input buffer, we route the
   // tokenization process to a specific lexeme.
-  // We require that the length of our buffer is at least four
-  // characters, else the behaviour of this function is undefined.
+  // We require that the length of our buffer is at least four characters, else
+  // the behaviour of this function is undefined.
 
   if (ctx->sz < 4) __builtin_unreachable();
 
