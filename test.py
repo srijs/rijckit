848b12bc2662208f5c7b7a8449ace32233587054
Sam Rijs <sam@dev.local>
2013-01-05 18:13:06 +0000

    make linter happy. whatever.

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 ffi = FFI()
 
 with open('lex.hi') as header:
-  ffi.cdef(header.read())
+    ffi.cdef(header.read())
 
 TOKENS = []
 
@@ -24,45 +24,45 @@ ctx.sz = stdin.readinto(ffi.buffer(buf, ctx.back_sz))
 
 run = True
 while run:
-  ret = Lex.lex(ctx, tok)
+    ret = Lex.lex(ctx, tok)
 
-  def refill():
-    ctx.sz += stdin.readinto(ffi.buffer(ctx.buf + ctx.sz, ctx.back_sz - ctx.sz))
-    while ctx.sz < 4:
-      ctx.buf[ctx.sz] = '\0'
-      ctx.sz += 1
+    def refill():
+        ctx.sz += stdin.readinto(ffi.buffer(ctx.buf + ctx.sz, ctx.back_sz - ctx.sz))
+        while ctx.sz < 4:
+            ctx.buf[ctx.sz] = '\0'
+            ctx.sz += 1
 
-  if ret.state == 'Success':
-    string = bufRD[tok.ptr - ctx.back_buf :
-                   tok.ptr - ctx.back_buf + tok.len].encode('string-escape')
-    TOKENS.append((copy(tok.type), string, tok.len, ret.t))
+    if ret.state == 'Success':
+        string = bufRD[tok.ptr - ctx.back_buf:
+                       tok.ptr - ctx.back_buf + tok.len].encode('string-escape')
+        TOKENS.append((copy(tok.type), string, tok.len, ret.t))
 
-  elif ret.state == 'Undecided':
-    if ctx.sz < ctx.back_sz:
-      refill()
-    else:
-      run = False
+    elif ret.state == 'Undecided':
+        if ctx.sz < ctx.back_sz:
+            refill()
+        else:
+            run = False
 
-  elif ret.state == 'Fail':
-    print 'Fail, Tok: %s, Char: %s' % (tok.type, ctx.buf[0])
-    run = False
+    elif ret.state == 'Fail':
+        print 'Fail, Tok: %s, Char: %s' % (tok.type, ctx.buf[0])
+        run = False
 
-  elif ret.state == 'End':
-    run = False
+    elif ret.state == 'End':
+        run = False
 
-x_length     = []
-y_cycles     = []
+x_length = []
+y_cycles = []
 y_throughput = []
-z_type       = []
+z_type = []
 
 for t in TOKENS:
-  print '%s: %s %d %d' % t
-  x_length.append(t[2])
-  y_cycles.append(t[3])
-  y_throughput.append(t[3] / t[2])
-  z_type.append(t[0])
+    print '%s: %s %d %d' % t
+    x_length.append(t[2])
+    y_cycles.append(t[3])
+    y_throughput.append(t[3] / t[2])
+    z_type.append(t[0])
 
 print 'Scattering...'
-plt.scatter(x_length, y_cycles, c = 'b')
-plt.scatter(x_length, y_throughput, c = 'r')
+plt.scatter(x_length, y_cycles, c='b')
+plt.scatter(x_length, y_throughput, c='r')
 plt.show()
